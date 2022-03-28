8538dc7929f6afe1c403317ec62f549815f0c21a
Ilya Tampio <quakumei@gmail.com>
2022-03-28 14:32:46 +0300

    fix namespace in ExpressionCalculator

--- cpp/S1_dev/src/ExpressionCalculator.h
+++ cpp/S1_dev/src/ExpressionCalculator.h
@@ -4,18 +4,23 @@
 #include <string>
 #include "Queue.hpp"
 
-class ExpressionCalculator
+namespace tampio 
 {
-public:
-    ExpressionCalculator(const std::string &);
-    long long solve() const;
-    Queue<std::string> postfixForm(Queue<std::string>) const;
+    class ExpressionCalculator
+    {
+    public:
+        ExpressionCalculator(const std::string &);
+        long long solve() const;
+        Queue<std::string> postfixForm(Queue<std::string>) const;
 
-private:
-    void exprToLexemes(const std::string &);
-    Queue<std::string> lexemes_;
-    static int order(const char &);
-    static bool isHigher(const char &, const char &);
-};
+    private:
+        void exprToLexemes(const std::string &);
+        Queue<std::string> lexemes_;
+        static int order(const char &);
+        static bool isHigher(const char &, const char &);
+    };
+}
+
+
 
 #endif
