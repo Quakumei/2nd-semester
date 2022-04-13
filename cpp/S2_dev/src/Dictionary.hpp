788551efab13aa1bf0cf63ea35e3995adc1619b7
Ilya Tampio <quakumei@gmail.com>
2022-04-13 19:29:54 +0300

    Formatter set up

---
+++ cpp/S2_dev/src/Dictionary.hpp
@@ -0,0 +1,22 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <utility>
+#include "ForwardList.hpp"
+
+namespace tampio
+{
+  template < typename Key, typename Value, typename Compare >
+  class Dictionary
+  {
+  public:
+    void push(Key k, Value v);
+    Value get(Key k);
+    Value drop(Key k);
+
+  private:
+    ForwardList< std::pair< Key, Value > > listData_;
+  };
+} // namespace tampio
+
+#endif
---
+++ cpp/S2_dev/src/ForwardList.hpp
@@ -0,0 +1,13 @@
+#ifndef FORWARDLIST_H
+#define FORWARDLIST_H
+
+namespace tampio
+{
+  template < typename T >
+  class ForwardList
+  {
+  public:
+  };
+} // namespace tampio
+
+#endif
