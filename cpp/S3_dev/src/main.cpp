f74c9f57beaf624592c737ed601ee86ae520c7fd
Ilya Tampio <quakumei@gmail.com>
2022-05-13 16:12:30 +0300

    S3 interface ready to be developed

---
+++ cpp/S3_dev/src/main.cpp
@@ -0,0 +1,97 @@
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include "BidirectionalList.hpp"
+#include "Dictionary.hpp"
+#include "utility.hpp"
+
+using namespace tampio;
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "No input file specified";
+    return 1;
+  }
+  std::ifstream in(argv[1]);
+
+  using Dict = Dictionary< std::string, tampio::BidirectionalList< long > >;
+  ForwardList< std::string > lexemes;
+  Dict lists;
+  while (!in.eof())
+  {
+    std::string line = "";
+    std::getline(in, line);
+    if (line != "\0")
+    {
+      line += ' ';
+      std::string temp = "";
+      for (size_t i = 0; i < line.size(); i++)
+      {
+        if (line[i] == ' ')
+        {
+          lexemes.pushBack(temp);
+          temp = "";
+        }
+        else
+        {
+          temp += line[i];
+        }
+      }
+      std::string listName = lexemes.front();
+      lexemes.deleteFront();
+      BidirectionalList< long > list;
+      while (!lexemes.empty())
+      {
+        std::string value = lexemes.front();
+        lexemes.deleteFront();
+        list.pushBack(std::stol(value));
+      }
+      lists.push(listName, list);
+    }
+  }
+  while (!std::cin.eof())
+  {
+    try
+    {
+
+      std::string command = "";
+      std::cin >> command;
+      if (command == "print")
+      {
+        std::string name;
+        std::cin >> name;
+        BidirectionalList< long > example = lists.get(name);
+        print(example, name, std::cout);
+      }
+      // else if (command == "complement")
+      // {
+      //   std::string name, left, right;
+      //   std::cin >> name >> left >> right;
+      //   datasets.push(name, complement(datasets.get(left), datasets.get(right)));
+      // }
+      // else if (command == "intersect")
+      // {
+      //   std::string name, left, right;
+      //   std::cin >> name >> left >> right;
+      //   datasets.push(name, intersect(datasets.get(left), datasets.get(right)));
+      // }
+      // else if (command == "union")
+      // {
+      //   std::string name, left, right;
+      //   std::cin >> name >> left >> right;
+      //   datasets.push(name, unionDict(datasets.get(left), datasets.get(right)));
+      // }
+      else if (!std::cin.eof())
+      {
+        throw std::logic_error("unknown command");
+      }
+    }
+    catch (const std::exception &)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+}
---
+++ cpp/S3_dev/src/utility.hpp
@@ -0,0 +1,19 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <ostream>
+#include <string>
+#include "BidirectionalList.hpp"
+#include "Dictionary.hpp"
+
+namespace tampio
+{
+  using Dict = tampio::Dictionary< std::string, std::string >;
+  using DictOfDict = tampio::Dictionary< std::string, Dict >;
+
+  void print(const BidirectionalList< long >& d, const std::string& name, std::ostream& os);
+  Dict complement(const Dict&, const Dict&);
+  Dict intersect(const Dict&, const Dict&);
+  Dict unionDict(const Dict&, const Dict&);
+}
+#endif
